f10b91dd24ea8322d7beccfe1c301a2861dc876d
PsyBrainy <martuto93@gmail.com>
2021-04-07 17:23:33 -0300

    agrege nuevo servicio! invoice-service, solo init y configuracion.

--- invoice-service/src/main/java/com/psybrainy/invoice/InvoiceServiceApplication.java
+++ invoice-service/src/main/java/com/psybrainy/invoice/InvoiceServiceApplication.java
@@ -1,4 +1,4 @@
-package com.psybrainy.product;
+package com.psybrainy.invoice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ProductServiceApplication {
+public class InvoiceServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
+		SpringApplication.run(InvoiceServiceApplication.class, args);
 	}
 
 }
